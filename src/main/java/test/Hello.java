2689a0c5597a058f1ecc51568cebe5642e88cccd
vou <voutax@gmail.com>
2021-09-19 09:46:10 +0800

    Initial commit

---
+++ src/main/java/test/Hello.java
@@ -0,0 +1,30 @@
+package test;
+
+import javax.lang.model.SourceVersion;
+import javax.print.DocFlavor;
+import javax.print.StreamPrintServiceFactory;
+import javax.xml.transform.SourceLocator;
+import javax.xml.xpath.XPathFunctionResolver;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello my friend");
+        ArrayList<Integer> al = new ArrayList<>();
+        //sss
+        String v = "我去了";
+        System.out.println("v = " + v);
+
+        List<String> l = new ArrayList<>();
+        l.add("xx");
+        l.add("haojiahujo");
+
+
+    }
+
+
+
+}
